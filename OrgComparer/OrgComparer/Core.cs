8609523f42e372b99427e55d754af054840a98e5
Evgeniy Kosjakov <42_@mail.ru>
2016-04-26 12:53:51 +0300

    console app added

---
+++ OrgComparer/OrgComparer/Core.cs
@@ -0,0 +1,14 @@
+﻿using Octokit;
+
+namespace OrgComparer
+{
+    public class Core
+    {
+        public void Test()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+
+        }
+
+    }
+}
